1af77af881c1276944078cd75d5da777991ca677
unknown <Administrator@ST10725979-SLAB.COM>
2023-09-30 13:49:18 +0530

    Created GET mapping to fetch all book

---
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.bookstore.controller;
+
+// Packages
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.employee.model.Employee;
+
+
+// Controller Implementation
+
+//@CrossOrigin(origins = "http://localhost:8093")
+@RestController
+@RequestMapping("/bookinfo")
+public class BookController {
+	
+	//Connecting to book service interface
+	@Autowired
+	Book bookservice;
+	
+	
+	// Implementation to get info related to all books
+	@GetMapping("/gettAllBook")
+	public List<Book> getAllBook(){
+		
+		System.out.println("Retreiving All Book Info.....");
+		List<Book> booklist = bookservice.showAllBook();
+		return booklist.isEmpty()?Collections.emptyList():booklist;
+	}
+
+}
+
+
+
