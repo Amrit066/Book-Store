b08d98ace38fa3e12699ff82c52be971a858f2c8
Taha-LTI <144654954+Taha-LTI@users.noreply.github.com>
2023-10-03 01:21:53 -0700

    Added book database functionality under /bookdb/
    Uploaded the entire project under this new folder

---
+++ BookDB/src/main/java/com/scenario/book/controller/BookController.java
@@ -0,0 +1,88 @@
+package com.scenario.book.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scenario.book.model.Book;
+import com.scenario.book.service.BookService;
+
+@RestController
+@RequestMapping("/bookinfo")
+public class BookController {
+
+	// Connecting to book service interface
+	@Autowired
+	BookService bookservice;
+
+	// Implementation to get info related to all books
+	@GetMapping("/getAllBook")
+	public List<Book> getAllBook() {
+
+		System.out.println("Retreiving All Book Info.....");
+		List<Book> booklist = bookservice.getAllBooks();
+		return booklist.isEmpty() ? Collections.emptyList() : booklist;
+	}
+
+	// Implementation to get info related to specific books
+	// Based on id
+	@PutMapping("/getBookById/{id}")
+	public Book getBookById(@PathVariable("id") int id) {
+		return bookservice.getBookById(id);
+	}
+
+	// Based on title
+	@GetMapping("/getBookByTitle/{title}")
+	public List<Book> getBookByTitle(@PathVariable("title") String title) {
+
+		System.out.println("Retreiving Book Info with title -" + title);
+		List<Book> booklist = bookservice.searchBooks("title", title);
+		return booklist.isEmpty() ? Collections.emptyList() : booklist;
+	}
+
+	// Based on author
+	@GetMapping("/getBookByAuthor/{author}")
+	public List<Book> getBookByAuthor(@PathVariable("author") String author) {
+
+		System.out.println("Retreiving Book Info with autor -" + author);
+		List<Book> booklist = bookservice.searchBooks("author", author);
+		return booklist.isEmpty() ? Collections.emptyList() : booklist;
+	}
+
+	// Based on author
+	@GetMapping("/getBookByPrice/{price}")
+	public List<Book> getBookByPrice(@PathVariable("price") String price) {
+
+		System.out.println("Retreiving Book Info with price -" + price);
+		List<Book> booklist = bookservice.searchBooks("price", price);
+		return booklist.isEmpty() ? Collections.emptyList() : booklist;
+	}
+
+	// Implementation related to add books
+	@PostMapping("/addNewBook")
+	public Book addNewBook(@RequestBody Book b) {
+		return bookservice.addBook(b);
+	}
+
+	// Implementation related to update book details
+	@PutMapping("/updateBook/{id}")
+	public Book updateBook(@PathVariable("id") int id, @RequestBody Book b) {
+		return bookservice.updateBook(id, b);
+	}
+
+	// Implementation related to delete specific book
+	@DeleteMapping("/deleteBook/{id}")
+	public List<Book> deleteBook(@PathVariable("id") int id) {
+		return bookservice.deleteBook(id);
+	}
+
+}
---
+++ BookDB/src/main/java/com/scenario/book/model/Book.java
@@ -0,0 +1,84 @@
+package com.scenario.book.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+/*
+ * Entity Class for the Book table, which will connect to the database
+ * 
+ */
+
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int Id;
+	private String title;
+	private String author;
+	private String description;
+	private float price;
+	private int count;
+	
+	public Book() {
+		
+	}
+	//Parameterized Constructor
+	public Book(int id, String title, String author, String description, float price, int count) {
+		super();
+		Id = id;
+		this.title = title;
+		this.author = author;
+		this.description = description;
+		this.price = price;
+		this.count = count;
+	}
+	
+	//Getters and Setters
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public long getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [Id=" + Id + ", title=" + title + ", author=" + author + ", description=" + description
+				+ ", price=" + price + ", count=" + count + "]";
+	}
+	
+}
